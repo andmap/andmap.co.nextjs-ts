c3a10a8a30eb9112e0c6fc9166b7e897f4e79505
Jun Wang <andmap.co@gmail.com>
2022-05-13 07:56:39 -0700

    feat: new foot

---
+++ components/Foot.tsx
@@ -0,0 +1,93 @@
+import Image from 'next/image'
+import InstagramIcon from './svg-components/InstagramIcon'
+import TwitterIcon from './svg-components/TwitterIcon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 bg-veryLightBlue p-16'>
+        <div className='md:col-span-2 lg:col-span-3'>
+          <Image
+            src={'/assets/andmap.logo.png'}
+            alt='logo'
+            width={64}
+            height={20}
+          />
+          <div className='text-sm text-fontGray'>© 2022 Andmap LLC.</div>
+          <div className='text-sm text-fontGray'>
+            - Developer of software solutions -
+          </div>
+        </div>
+        <div>
+          <div className=' uppercase text-sm mt-4 mb-4'>
+            CUSTOMIZED SOLUTIONS
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>Suwasa</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>Optus</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>
+              OCR Mortgage Documents
+            </span>
+          </div>
+        </div>
+        <div>
+          <div className=' uppercase text-sm mt-4 mb-4'>QUICK LINKS</div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>Contact</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>About</span>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: 12 }} />{' '}
+            <span className='text-sm text-gray-500 '>Latest Projects</span>
+          </div>
+        </div>
+        <div>
+          <div className=' uppercase text-sm mt-4 mb-4'>FOLLOW US</div>
+
+          <div className='md:flex-auto md:flex-row mt-1 flex-row flex'>
+            <a href='/#' className='w-6 mx-1'>
+              <TwitterIcon className='hover:cursor-pointer hover:fill-blue fill-brightBlue ' />
+            </a>
+            <a href='/#' className='w-6 mx-1'>
+              <InstagramIcon className='hover:cursor-pointer hover:fill-blue fill-brightBlue ' />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/*}
+      <div className='bg-veryLightBlue pt-2 '>
+        <div
+          className='flex pb-4 px-3 m-auto pt-4 border-t text-gray-800 text-sm flex-col
+     md:flex-row max-w-6xl'
+        >
+          <div>© 2022 Andmap LLC.</div>
+
+          <div className='md:flex-auto md:flex-row-reverse mt-1 flex-row flex'>
+            <a href='/#' className='w-6 mx-1'>
+              <TwitterIcon className='hover:cursor-pointer hover:fill-blue fill-brightBlue ' />
+            </a>
+            <a href='/#' className='w-6 mx-1'>
+              <InstagramIcon className='hover:cursor-pointer hover:fill-blue fill-brightBlue ' />
+            </a>
+          </div>
+        </div>
+      </div>
+  */}
+    </div>
+  )
+}
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+config.autoAddCss = false
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
